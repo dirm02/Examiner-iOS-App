4f867736f4e83ef07e3b80cc0657d0f61a697289
monta <magneticraider@gmail.com>
2024-08-07 05:17:44 -0700

    Update UI design

--- src/app/case-details/case-details.page.ts
+++ src/app/case-details/case-details.page.ts
@@ -1,20 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonIcon, IonBackButton,
+        IonButtons } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-case-details',
   templateUrl: './case-details.page.html',
   styleUrls: ['./case-details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,
+            IonButton, IonIcon, IonBackButton, IonButtons]
 })
 export class CaseDetailsPage implements OnInit {
 
-  constructor() { }
+  caseDetails = {
+    id: 'CaseAndroidTest-102822-35',
+    patientName: 'Sarah Smith',
+    mrn: '0114151',
+    status: 'In Progress',
+    dateTime: '01/02/22 05:24',
+    examiner: 'Holly Jacobs',
+    location: 'USC Medical Center, Los Angeles, CA',
+    images: [
+      'assets/Screenshot_4.png',
+      'assets/Screenshot_4.png',
+      'assets/Screenshot_4.png',
+      'assets/Screenshot_4.png',
+    ],
+    videos: 1
+  };
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  deleteCase() {
+    // Implement delete logic here
+    console.log('Case deleted');
+  }
+
+  completeCase() {
+    // Implement complete logic here
+    console.log('Case completed');
   }
 
 }
--- src/app/media-review/media-review.page.ts
+++ src/app/media-review/media-review.page.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCol, IonRow, IonGrid, IonBackButton,
+        IonButtons } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-media-review',
   templateUrl: './media-review.page.html',
   styleUrls: ['./media-review.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,
+            IonCol, IonRow, IonGrid, IonBackButton, IonButtons]
 })
 export class MediaReviewPage implements OnInit {
 
-  constructor() { }
+  galleryItems: Array<{ src: string }>;
 
-  ngOnInit() {
+  constructor() {
+    this.galleryItems = [
+      { src: 'assets/Screenshot_4.png' },
+      { src: 'assets/Screenshot_4.png' },
+      { src: 'assets/Screenshot_4.png' },
+      { src: 'assets/Screenshot_4.png' }
+    ];
   }
+
+  ngOnInit() {}
 
 }
